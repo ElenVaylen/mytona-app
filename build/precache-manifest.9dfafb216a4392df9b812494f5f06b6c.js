40948b342f3deb8f0370846da53335d4287a8a13
Casterly <elen.vaylen@gmail.com>
2020-05-17 23:48:34 +0300

    add build

---
+++ build/precache-manifest.9dfafb216a4392df9b812494f5f06b6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0dca9d33306f1125b2ee796379d008f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5cd7f05e54f5edb2fb22",
+    "url": "/static/css/main.08d0178b.chunk.css"
+  },
+  {
+    "revision": "104fb42ef456332c4d4a",
+    "url": "/static/js/2.da1be932.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.da1be932.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cd7f05e54f5edb2fb22",
+    "url": "/static/js/main.a4c2729f.chunk.js"
+  },
+  {
+    "revision": "1aa69148861d3f81a619",
+    "url": "/static/js/runtime-main.620a695b.js"
+  },
+  {
+    "revision": "9b4795177da740012c7da7b4db987291",
+    "url": "/static/media/bg2.9b479517.jpg"
+  }
+]);
